1ebe0d58e12cf1b4815d573df5517657e0113f82
Nikita35-a11y <124081198+Nikita35-a11y@users.noreply.github.com>
2023-11-03 12:42:30 +0400

    Update parser_avito.py

--- parser_avito.py
+++ parser_avito.py
@@ -16,26 +16,62 @@ dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
+    kb = [
+        [types.KeyboardButton(text="/start")],
+        [types.KeyboardButton(text="/end")],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
+
+    await message.answer("Привет!\n Я Slice, бот для быстрого поиска бу,\n а также товаров по низким ценам 🤖")
+    await message.answer("Напиши название товара или услуги 🛒", reply_markup=keyboard)
 
 
-@dp.message(Command("price"))
+@dp.message(Command("end"))
+async def cmd_start(message: types.Message):
+    kb = [
+        [types.KeyboardButton(text="/end")],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
+    await message.answer("До встречи!\nРад был помочь 😉", reply_markup=keyboard)
+
+
+@dp.message()
 async def get_info(message: types.Message):
-    await message.answer("Напиши название товара или услуги 🛒")
-
     if message.text != "" or message.text != " ":
-        url_avito = f"https://www.avito.ru/izhevsk?q={message.message_id, message.text}&s=1"
+        url_avito = f"https://www.avito.ru/all?q={message.text}&s=1"
         html = requests.get(url_avito).text
         bs = BeautifulSoup(html)
 
-        info_avito = {'Цена 💸': [0, 0], 'Название📋': [0, 0]}
+        ebay = f"https://www.ebay.com/sch/i.html?_from=R40&_nkw={message.text}&_sacat=0&_sop=15"
+        html_ebay = requests.get(ebay).text
+        bs_ebay = BeautifulSoup(html_ebay)
+
+        info_ebay = {"Цена💸": [0, 0], "Название📋": [0, 0]}
+        pd_ebay = pd.DataFrame(info_ebay)
+
+        info_avito = {'Цена💸': [0, 0], 'Название📋': [0, 0]}
         data_avito = pd.DataFrame(info_avito)
 
         all_link = bs.find_all("div", {"class": "iva-item-priceStep-uq2CQ"})
         a = bs.find_all("h3", {"class": "styles-module-root-TWVKW"})
 
+        n = bs_ebay.find_all("div", {"class": "s-item__title"})
+        pc = bs_ebay.find_all("span", {"class": "s-item__price"})
+
         prices_avito = []
         names_avito = []
+
+        names_ebay = []
+        prices_ebay = []
+
+        for el2 in n:
+            names_ebay.append(el2.text)
+            print(el2.text)
+
+        for el2 in pc:
+            k = "".join(c2 for c2 in el2.text if c2.isdigit())
+            print(k)
+            prices_ebay.append(f"{int(k)}")
 
         for el in all_link:
             b = "".join(c for c in el.text if c.isdigit())
@@ -57,11 +93,15 @@ async def get_info(message: types.Message):
         info_avito = {'Цена 💸': prices_avito[:5], 'Название 📋': names_avito[:5]}
         data_avito = pd.DataFrame(info_avito)
 
+        info_ebay = {"Цена💸": prices_ebay[:5], "Название📋": names_ebay[:5]}
+        pd_ebay = pd.DataFrame(info_ebay)
+
         answer = "\n"
         if 0 in prices_avito:
             answer += "\n⚠Предупреждение! Чаще всего товары с ценой 0 плохие по качеству или же имеют договорную цену!⚠"
 
-        await message.answer(f"{data_avito} {answer}")
+        await message.answer(f"{data_avito} {answer}\n")
+        await message.answer(f"{pd_ebay} {answer}")
     else:
         await message.answer("⚠Неверное имя товара!⚠")
 
